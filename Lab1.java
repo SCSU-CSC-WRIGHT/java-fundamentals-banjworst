a9de5e4ef75ebd045258255654dec52fbe46d505
banjworst <sutherlands3@southernct.edu>
2025-02-05 17:36:57 -0500

    Finish Exercise 4

---
+++ Lab1.java
@@ -0,0 +1,22 @@
+public class Lab1 {
+    public static void main(String[] args) { 
+
+        //Exercise 1
+        System.out.println("Hello, World!");
+
+        //Exercise 2 
+       int num = 10; 
+       String message = "The number is";
+       System.out.println(message + num);
+
+       //Exercise 3 
+       if(num % 2 == 0){
+            System.out.println("Even");
+       }else{ 
+            System.out.println("Odd");
+       }
+       }
+
+       //Exercise 4 
+       for (int i = 0;)
+    }
